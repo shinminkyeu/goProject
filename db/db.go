f2d547a4fc028780362230cb2b6a58d02fd39895
Felix9407 <znzno700@wemade.com>
2020-03-04 17:29:24 +0900

    commit

--- db/db.go
+++ db/db.go
@@ -2,70 +2,124 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"math/big"
+	"sync"
 	"time"
 
-	"github.com/klaytn/klaytn/common"
+	"github.com/klaytn/klaytn/blockchain/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-type BlockData struct {
-	Number       uint64
-	TxSha        common.Hash
-	Time         uint64
-	Transactions string
+type BlockHeader struct {
+	Number       uint64    `bson: number`
+	Root         string    `bson: root`
+	GasUsed      uint64    `bson: gasused`
+	ParentHash   string    `bson: parenthash`
+	TxHash       string    `bson: txhash`
+	ReceiptHash  string    `bson: receipthash`
+	BlockTime    uint64    `bson: blocktime`
+	Time         time.Time `bson: time`
+	Transactions []TxInfo  `bson: transactions`
+}
+
+type TxInfo struct {
+	Hash     string `bson: hash`
+	Type     string `bson: type`
+	From     string `bson: from`
+	To       string `bson: to`
+	Value    uint64 `bson: value`
+	Nonce    uint64 `bson: nonce`
+	GasPrice uint64 `bson: gasprice`
+	Gas      uint64 `bson: gaslimit`
+	Data     []byte `bson: data`
 }
 
 type DB struct {
-	client *mongo.Client
-	err    chan error
+	client     *mongo.Client
+	collection *mongo.Collection
 }
 
-func GetDB() *DB {
+func GetDB() (*DB, error) {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 
-	inst := &DB{
-		client: client,
-		err:    make(chan error),
+	if err != nil {
+		return nil, err
 	}
-	inst.err <- err
-	return inst
+	collection := client.Database("Klaytn").Collection("Block")
+	reVal := &DB{
+		client:     client,
+		collection: collection,
+	}
+	return reVal, err
 }
 
-func (db *DB) GetLastBlock() *big.Int {
-	//db.client
+func (db *DB) GetLastBlock() (*big.Int, error) {
+	rec := BlockHeader{}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := db.client.Connect(ctx)
-	db.err <- err
-	err = db.client.Ping(ctx, readpref.Primary())
-	db.err <- err
-	err = db.client.Disconnect(context.TODO())
-	db.err <- err
-	return new(big.Int).SetUint64(21586582)
+	option := options.FindOne().SetSort(map[string]int{"number": -1})
+	cur := db.collection.FindOne(ctx, bson.D{}, option)
+	err = cur.Decode(&rec)
+	if err != nil {
+		return new(big.Int).SetUint64(0), err
+	}
+	fmt.Println("hello ", new(big.Int).SetUint64(rec.Number))
+	return new(big.Int).SetUint64(rec.Number), err
 }
 
-func (db *DB) InsertBlock(data *BlockData) {
-	//client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+func (db *DB) InsertBlock(data *types.Block) {
+	blockData := &BlockHeader{
+		Number:      data.Number().Uint64(),
+		Root:        data.Root().String(),
+		GasUsed:     data.GasUsed(),
+		ParentHash:  data.ParentHash().String(),
+		TxHash:      data.TxHash().String(),
+		ReceiptHash: data.ReceiptHash().String(),
+		BlockTime:   data.Time().Uint64(),
+		Time:        time.Now(),
+	}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := db.client.Connect(ctx)
-	db.err <- err
-	err = db.client.Ping(ctx, readpref.Primary())
-	db.err <- err
-	collection := db.client.Database("Klaytn").Collection("Block")
-	_, err = collection.InsertOne(context.TODO(), data)
-	err = db.client.Disconnect(context.TODO())
-	db.err <- err
+
+	len := data.Transactions().Len()
+	if len > 0 {
+		wg := sync.WaitGroup{}
+		wg.Add(len)
+		txChan := make(chan *TxInfo, len)
+		for _, data := range data.Transactions() {
+			go db.InsertTransaction(data, txChan, &wg)
+		}
+		wg.Wait()
+		close(txChan)
+		transactions := make([]TxInfo, len)
+		for i := 0; i < len; i++ {
+			transactions[i] = *<-txChan
+		}
+		blockData.Transactions = transactions
+		_, err = db.collection.InsertOne(ctx, *blockData)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
 
-func (db *DB) UseDB(logic func()) {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := db.client.Connect(ctx)
-	db.err <- err
-	err = db.client.Ping(ctx, readpref.Primary())
-	db.err <- err
-	logic()
-	err = db.client.Disconnect(context.TODO())
-	db.err <- err
+func (db *DB) InsertTransaction(data *types.Transaction, txs chan *TxInfo, wg *sync.WaitGroup) {
+	from, _ := data.From()
+	tx := &TxInfo{
+		Hash:     data.Hash().String(),
+		Type:     data.Type().String(),
+		From:     from.String(),
+		To:       data.To().String(),
+		Value:    data.Value().Uint64(),
+		Nonce:    data.Nonce(),
+		GasPrice: data.GasPrice().Uint64(),
+		Gas:      data.Gas(),
+		Data:     data.Data(),
+	}
+	txs <- tx
+	wg.Done()
 }
--- main.go
+++ main.go
@@ -1,57 +1,53 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"math/big"
 	"time"
 
-	"github.com/scanBlock/goKlaytn/block"
-	"github.com/scanBlock/goKlaytn/db"
+	"github.com/scanBlock/goProject/block"
+	"github.com/scanBlock/goProject/db"
 )
 
 type Info struct {
-	klaytnBlockNumber *big.Int
-	dbBlockNumber     *big.Int
+	chainBlockNumber *big.Int
+	dbBlockNumber    *big.Int
 }
 
 func main() {
 	chain, err := block.GetChain()
-	db := db.GetDB()
+	db, err := db.GetDB()
 	if err != nil {
 		fmt.Println("err : ", err)
 	}
+	dbLastBlock, _ := db.GetLastBlock()
 	inst := &Info{
-		klaytnBlockNumber: chain.GetLastBlock(),
-		dbBlockNumber:     db.GetLastBlock(),
+		chainBlockNumber: dbLastBlock,
+		dbBlockNumber:    dbLastBlock,
 	}
 	inst.loop(chain, db)
 }
 
 func (i *Info) loop(p *block.Chain, q *db.DB) {
 	//go func() {
-	//	for {
-	if blockNumber := p.GetLastBlock(); blockNumber == i.klaytnBlockNumber {
-		time.Sleep(0.1e9)
-	} else {
-		i.klaytnBlockNumber = blockNumber
-	}
-	if _block, err := p.GetBlockByNumber(i.klaytnBlockNumber); err == nil {
-		TransactionsStr, err := json.Marshal(_block.Transactions())
-		if err != nil {
-			fmt.Println("err : ", err)
-			return
+	for {
+		if blockNumber := p.GetLastBlock(); blockNumber.Cmp(i.chainBlockNumber) == 0 {
+			time.Sleep(0.1e9)
+		} else {
+			i.chainBlockNumber = blockNumber
 		}
-		block := &db.BlockData{
-			Number:       _block.NumberU64(),
-			TxSha:        _block.TxHash(),
-			Time:         _block.Time().Uint64(),
-			Transactions: string(TransactionsStr),
+		fmt.Println("i.chainBlockNumber : ", i.chainBlockNumber)
+		if i.chainBlockNumber.Cmp(i.dbBlockNumber) > 0 {
+			i.dbBlockNumber.Add(i.dbBlockNumber, new(big.Int).SetUint64(1))
+			if _block, err := p.GetBlockByNumber(i.dbBlockNumber); err == nil {
+				go q.InsertBlock(_block)
+				fmt.Println("i.dbBlockNumber : ", i.dbBlockNumber)
+			} else {
+				fmt.Println("err : ", err)
+			}
+		} else {
+			time.Sleep(0.1e9)
 		}
-		q.InsertBlock(block)
-	} else {
-		fmt.Println("err : ", err)
 	}
-	//	}
 	//}()
 }
